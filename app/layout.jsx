c7af08b6e37637db011191bc91f234f9beb08236
centinela1024tb <centinela1024tb@gmail.com>
2023-09-02 13:33:32 -0600

    [ADD] add font to GoggleFont with nextJS

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,15 @@
 import NavBar from '../components/Navbar'
 
+import {
+  Roboto
+} from 'next/font/google'
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  style: ["italic", "normal"],
+  subsets: ["latin"]
+});
+
 export const metadata = {
   title: "My Store with NextJS - HomePage",
   description: "Esta e sla pagina principal del site",
@@ -9,7 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
  return (
     <html lang="en">
-      <body>
+      <body className={roboto.className}>
         <NavBar />
 
         {children}
